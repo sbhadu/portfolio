46caf5369759f52a434e3459edf7f6e13c03f3d3
sahil bhadu <70458053+sbhadu@users.noreply.github.com>
2022-02-23 15:32:01 +0530

    change

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, I'm Sahil Bhadu.
-                        <br className="hidden lg:inline-block" />I love to build creative ideas and apps
+                        <br className="hidden lg:inline-block" /> I love to build creative ideas and apps
                     </h1>
                     <p className="mb-8 leading-relaxed">
                         Recently I'm a computer science engineering student at NIT Trichy(2018-2022). Incoming developer associate at SAP Labs India.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import './footer.css'
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 text-white">
+        <footer className="bg-gray-100 text-black">
             <hr />
-            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="bg-gray-100 relative flex flex-wrap py-6 rounded shadow-md">
                 <div className="lg:w-1/2 px-6">
                     <span>Made with ❤ by Sahil Bhadu.</span>
                 </div>
